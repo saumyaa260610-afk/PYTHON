7329b3a89b2154189d9840594213e28d8a70230b
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-06 21:01:13 +0530

    Update faculty.py

--- Projects/project-03-library-management-system/faculty.py
+++ Projects/project-03-library-management-system/faculty.py
@@ -2,5 +2,5 @@ from member import Member
 class Faculty(Member):
     limit_borrow=5
     loan_days=30
-    def __init__(self,user_id,name,password):
+    def __init__(self,user_id,name,password,"Faculty"):
         super().__init__(user_id,name,password)
