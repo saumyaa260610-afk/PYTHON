3d1d3cd2f3b05ecb354bebe7d3953e1de65a595c
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-06 20:42:20 +0530

    Update library.py

--- Projects/project-03-library-management-system/library.py
+++ Projects/project-03-library-management-system/library.py
@@ -141,3 +141,15 @@ class Library:
                 member.details()
             else:
                 print("Member not found")
+
+        def save(self):
+            all_data={}
+            all_data["books"]={}
+            all_data["members"]={}
+            for book_id,book in self.books.items():
+                all_data["books"][book_id] = {"title":book.title,"author":book.author,"year":book.year,"status":book.status,"borrowed_by":book.borrowed_by,"borrow_date":book.borrow_date,"return_by":book.return_by,"reserved_by": book.reserved_by}
+            for user_id,member in self.members.items():
+                all_data["members"][user_id]={"name":member.name,"password":member.password,"borrowed_books":member.borrowed_books,"reserved_books":member.reserved_books}
+            with open("library.json", "w") as f:
+                json.dump(all_data,f,indent=4)
+                print("Data saved")
