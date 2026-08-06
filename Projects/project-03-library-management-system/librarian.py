c421fac9d60e3c74e5174240f3f5a46cb2b62415
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-06 21:01:36 +0530

    Update librarian.py

--- Projects/project-03-library-management-system/librarian.py
+++ Projects/project-03-library-management-system/librarian.py
@@ -1,4 +1,4 @@
 from member import Member
 class Librarian(Member):
     def __init__(self,user_id,name,password):
-        super().__init__(user_id,name,password)
+        super().__init__(user_id,name,password,"Librarian")
