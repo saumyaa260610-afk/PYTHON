dbc01234958179c19055ebcf949e7ccb6bbf938c
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-10 20:55:56 +0530

    Update student.py

--- Projects/project-03-library-management-system/student.py
+++ Projects/project-03-library-management-system/student.py
@@ -3,5 +3,5 @@ class Student(Member):
     limit_borrow=3
     loan_days=15
     fine_each_day=10
-    def __init__(self,user_id,name,password,"Student"):
-        super().__init__(user_id,name,password)
+    def __init__(self,user_id,name,password):
+        super().__init__(user_id,name,password,"Student")
