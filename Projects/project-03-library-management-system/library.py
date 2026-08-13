3755d7107c90c9d40da43891b9e625b809d2b480
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-13 13:58:14 +0530

    Update library.py

--- Projects/project-03-library-management-system/library.py
+++ Projects/project-03-library-management-system/library.py
@@ -76,9 +76,6 @@ class Library:
             print("Book borrowed successfully")
             
         def return_book(self,user_id,book_id):
-            if user_id not in self.members:
-                print("Member not found")
-                return
             if book_id not in self.books:
                 print("Book not found")
                 return
