4d8dedc05b53c7933dbf57299a6ab2461f861c2c
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-22 13:15:23 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -7,14 +7,16 @@ class node:
 class linked_list:
     def __init__(self):
         self.head=None
+        self.tail=None
+        
     def add(self,key,value):
         new_node=node(key,value)
         if self.head is None:
             self.head=new_node
-        curr=self.head
-        while curr.next is not None:
-            curr=curr.next
-        curr.next=new_node
+            self.tail=new_node
+            return
+        self.tail.next=new_node
+        self.tail=new_node
         
     def find(self,key):
         curr=self.head
