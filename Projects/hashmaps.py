0f5a4a27d0c2c8eff30a777ddc55fadba17d498c
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-08-26 14:35:43 +0530

    Update hashmaps.py

--- Projects/hashmaps.py
+++ Projects/hashmaps.py
@@ -1,69 +1,54 @@
-class HashMap:
-    def __init__(self, size=10):
-        self.size = size
-        self.table = [[] for _ in range(size)]
-
-    def hash_function(self, key):
-        return hash(key) % self.size
-
-    def insert(self, key, value):
-        index = self.hash_function(key)
-
-        for pair in self.table[index]:
-            if pair[0] == key:
-                pair[1] = value
-                return
-
-        self.table[index].append([key, value])
-
-    def search(self, key):
-        index = self.hash_function(key)
-
-        for pair in self.table[index]:
-            if pair[0] == key:
-                return pair[1]
-
-        return None
-
-    def delete(self, key):
-        index = self.hash_function(key)
-
-        for pair in self.table[index]:
-            if pair[0] == key:
-                self.table[index].remove(pair)
-                return
+from linkedlist import LinkedList
 
 class HashMap:
-    def __init__(self, size=10):
-        self.size = size
-        self.table = [[] for _ in range(size)]
-
-    def hash_function(self, key):
-        return hash(key) % self.size
-
-    def insert(self, key, value):
-        index = self.hash_function(key)
-
-        for pair in self.table[index]:
-            if pair[0] == key:
-                pair[1] = value
-                return
-
-        self.table[index].append([key, value])
-
-    def search(self, key):
-        index = self.hash_function(key)
-
-        for pair in self.table[index]:
-            if pair[0] == key:
-                return pair[1]
-
-        return None
-
-    def delete(self, key):
-        index = self.hash_function(key)
-
-        for pair in self.table[index]:
-            if pair[0] == key:
-                self.table[index].remove(pair)
-                return
+    def __init__(self):
+        self.initial_bucket_size=10
+        self.bucketSize=self.initial_bucket_size
+        self.n_of_elements=0
+        self.buckets=[LinkedList() for _ in range(self.bucketSize)]
+    def hashKey(self,key):
+        if isinstance(key,str):
+            hash_value=5381
+            for c in key:
+                hash_value=((hash_value<<5)+hash_value)+ord(c)
+            return hash_value
+        elif isinstance(key,int):
+            hash_value=key
+            hash_value^=(hash_value>>20)^(hash_value>>12)
+            hash_value^=(hash_value>>7)^(hash_value>>4)
+            return hash_value
+        else:
+            raise TypeError("Unsupported key type")
+    def compress(self,hash_value):
+        return hash_value%self.bucketSize
+    def __getitem__(self,key):
+        index=self.compress(self.hashKey(key))
+        node=self.buckets[index].findNode(key)
+        if node is not None:
+            return node.val
+        if 4*self.n_of_elements>=3*self.bucketSize:
+            old_buckets=self.buckets
+            self.bucketSize=self.bucketSize*2
+            self.buckets=[LinkedList() for _ in range(self.bucketSize)]
+            self.n_of_elements=0
+            for chain in old_buckets:
+                temp=chain.returnhead()
+                while temp is not None:
+                    newidx=self.compress(self.hashKey(temp.key))
+                    self.buckets[newidx].addHead(temp.key,temp.val)
+                    self.n_of_elements+=1
+                    temp=temp.next
+        self.buckets[index].addHead(key,None)
+        self.n_of_elements+=1
+        return self.buckets[index].findNode(key).val
+    def erase(self,key):
+        index=self.compress(self.hashKey(key))
+        self.buckets[index].DeleteNode(key)
+    def getAll(self):
+        all_items=[]
+        for ll in self.buckets:
+            temp=ll.head
+            while temp is not None:
+                all_items.append((temp.key,temp.val))
+                temp=temp.next
+        return all_items
