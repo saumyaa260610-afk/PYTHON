8f4ce7a9021706318dabe027a67b67b0c60adb36
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-09-02 19:37:44 +0400

    Create Stack.py

---
+++ Projects/project-05-File Management system/Stack.py
@@ -0,0 +1,20 @@
+class stacknode:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class stack:
+    def __init__(self):
+        self.top=None
+    def push(self,value):
+        new_node=stacknode(value)
+        new_node.next=self.top
+        self.top=new_node
+    def pop(self):
+        if self.top is None:
+            return None
+        value=self.top.value
+        self.top=self.top.next
+        return value
+    def is_empty(self):
+        return self.top is None
