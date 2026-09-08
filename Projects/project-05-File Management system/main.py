cb131086f03ee882cb637493f008bcb4b6da72f5
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-09-08 20:42:55 +0400

    Update main.py

--- Projects/project-05-File Management system/main.py
+++ Projects/project-05-File Management system/main.py
@@ -1,38 +1,49 @@
 import heapq
+from Stack import stack
+from hashmap import HashMap
 
 def recentfiles(files,num):
     heap=[]
-    current=files.head
-    while current is not None:
-        filename=current.key
-        tree=current.value
-        heapq.heappush(heap, (tree.last_modified,filename))
+    for filename,tree in files.getAll():
+        heapq.heappush(heap,(tree.last_modified,filename))
         if len(heap)>num:
             heapq.heappop(heap)
-        current=current.next
-    result=[]
+    s=stack()
     while heap:
         item=heapq.heappop(heap)
-        result.append(item[1])
-    result.reverse()
-    for filename in result:
-        print(filename)
-
+        s.push(item[1])
+    while s.is_empty()==False:
+        print(s.pop())
+        
 def biggesttrees(files,num):
     heap=[]
-    current = files.head
-    while current is not None:
-        filename=current.key
-        tree=current.value
-        heapq.heappush(heap, (tree.version_count,filename))
+    for filename,tree in files.getAll():
+        heapq.heappush(heap,(tree.version_count,filename))
         if len(heap)>num:
             heapq.heappop(heap)
-        current=current.next
-    result=[]
+    s=stack()
     while heap:
         item=heapq.heappop(heap)
-        result.append(item[1])
-    result.reverse()
-    for filename in result:
-        print(filename)
+        s.push(item[1])
+    while s.is_empty()==False:
+        print(s.pop())
 
+#main prgrm
+files=HashMap()
+print("Welcome")
+while True:
+    print('''
+    MENU
+1. CREATE
+2. READ
+3. INSERT
+4. UPDATE
+5. SNAPSHOT
+6. ROLLBACK
+7. HISTORY
+8. RECENTFILES
+9. BIGGESTTREES
+10. EXIT ''')
+    choice=int(input("Enter choice:"))
+   
+        
