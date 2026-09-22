2e8ef9a7cfef9167f8227f5b3126944388628caf
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-09-22 16:53:11 +0400

    Update databases.py

--- Projects/project-03-library-management-system/databases.py
+++ Projects/project-03-library-management-system/databases.py
@@ -1,11 +1,12 @@
 import mysql.connector as db
+from datetime import date
 dbs=db.connect(host="localhost",user="root",password="pass",database="library")
 cur=dbs.cursor()
-    
+
 def create_table(query):
     cur.execute(query)
     dbs.commit()
-    
+
 table_books="""CREATE TABLE IF NOT EXISTS books(book_id INT PRIMARY KEY,title VARCHAR(100),author VARCHAR(100),year INT,status VARCHAR(20),borrowed_by INT,borrow_date DATE,return_by DATE,reserved_by INT)"""
 create_table(table_books)
 
@@ -29,13 +30,13 @@ def remove_book(book_id):
     dbs.commit()
 
 def add_member(user_id,name,password,role,fine):
-    query="""INSERT INTO members(user_id, name, password, role, fine) VALUES (%s,%s,%s,%s,%s)"""
+    query="""INSERT INTO members(user_id,name,password,role,fine) VALUES (%s,%s,%s,%s,%s)"""
     cur.execute(query,(user_id,name,password,role,fine))
     dbs.commit()
 
 def remove_member(user_id):
     query="DELETE FROM members WHERE user_id=%s"
-    cur.execute(query,(user_id))
+    cur.execute(query,(user_id,))
     dbs.commit()
 
 def update_book(book_id,title,author,year):
@@ -49,28 +50,28 @@ def update_member(user_id,name,password,role,fine):
     dbs.commit()
 
 def get_book(book_id):
-    query="""SELECT *FROM books WHERE book_id=%s"""
-    cur.execute(query,(book_id))
+    query="""SELECT * FROM books WHERE book_id=%s"""
+    cur.execute(query,(book_id,))
     return cur.fetchone()
 
 def get_all_books():
-    query="""SELECT*FROM books"""
+    query="""SELECT * FROM books"""
     cur.execute(query)
     return cur.fetchall()
 
 def get_member(user_id):
-    query="""SELECT*FROM members WHERE user_id=%s"""
-    cur.execute(query,(user_id))
+    query="""SELECT * FROM members WHERE user_id=%s"""
+    cur.execute(query,(user_id,))
     return cur.fetchone()
 
 def get_all_members():
-    query="""SELECT* FROM members"""
+    query="""SELECT * FROM members"""
     cur.execute(query)
     return cur.fetchall()
 
 def get_book_title(title):
-    query="SELECT* FROM books WHERE title=%s"
-    cur.execute(query,(title))
+    query="SELECT * FROM books WHERE title=%s"
+    cur.execute(query,(title,))
     return cur.fetchone()
 
 def borrow_book(book_id,user_id,borrow_date,return_by):
@@ -84,13 +85,13 @@ def return_book(book_id):
     dbs.commit()
 
 def reserve_book(book_id,user_id,reserve_date):
-    query="""INSERT INTO reserve(reserve_id,user_id,book_id,reserve_date) VALUES(%s,%s,%s,%s)"""
-    cur.execute(query,(reserve_id,user_id,book_id,reserve_date))
+    query="""INSERT INTO reserve(user_id,book_id,reserve_date) VALUES(%s,%s,%s)"""
+    cur.execute(query,(user_id,book_id,reserve_date))
     dbs.commit()
 
 def pay_fine(user_id,payment,remaining_fine):
-    query="""INSERT INTO payments(user_id,payment,remaining_fine) VALUES (%s,%s)"""
+    query="""INSERT INTO payments(user_id,amount,payment_date) VALUES(%s,%s,%s)"""
+    cur.execute(query,(user_id,payment,date.today()))
     query="UPDATE members SET fine=%s WHERE user_id=%s"
     cur.execute(query,(remaining_fine,user_id))
     dbs.commit()
-
