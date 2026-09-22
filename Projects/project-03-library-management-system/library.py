f18ec6862c00e4cc6978e90cda3c28840ba73948
saumyaa260610-afk <saumyaa260610@gmail.com>
2026-09-22 17:06:09 +0400

    Update library.py

--- Projects/project-03-library-management-system/library.py
+++ Projects/project-03-library-management-system/library.py
@@ -1,5 +1,5 @@
-from book import Book 
-from student import Student 
+from book import Book
+from student import Student
 from faculty import Faculty
 from librarian import Librarian
 from datetime import date,timedelta
@@ -7,116 +7,111 @@ from databases import *
 
 class Library:
     def __init__(self):
-        self.books={}
-        self.members={}
-        self.book_titles={}
         self.book_count=100
         self.member_count=100
 
     def login(self,user_id,password):
-        if user_id in self.members:
-            member=self.members[user_id]
-            if member.password==password:
-                print("Login Successful")
+        member=get_member(user_id)
+        if member:
+            if member[2]==password:
+                print("Login successful")
                 return member
             else:
-                print("Incorrect Password")
+                print("Incorrect password")
         else:
-            print("Invalid User ID")
-            return None
+            print("Invalid userID")
+        return None
 
     def add_book(self,title,author,year):
-            self.book_count+=1
-            book_id=self.book_count
-            new_book=Book(book_id,title,author,year)
-            self.books[book_id]=new_book
-            self.book_titles[title.lower()]=new_book
-            print("Book added successfully")
-            print("Book ID:",book_id)
-            add_book(book_id,title,author,year,new_book.status)
-        
+        self.book_count+=1
+        book_id=self.book_count
+        add_book(book_id,title,author,year,"Available")
+        print("Book added")
+        print("Book ID:",book_id)
+
     def remove_book(self,book_id):
-        if book_id not in self.books:
+        book=get_book(book_id)
+        if book==None:
             print("Book not found")
-        elif self.books[book_id].status!="Available":
-            print("Book cannot be removed")
+        elif book[4]!="Available":
+            print("Book is not available")
         else:
-            del self.books[book_id]
-            print("Book removed successfully")
             remove_book(book_id)
+            print("Book removed successfully")
 
     def search_book_id(self,book_id):
-        if book_id in self.books:
-            print(self.books[book_id])
+        book=get_book(book_id)
+        if book:
+            print(book)
         else:
             print("Book not found")
 
     def search_book_title(self,title):
-       if title.lower() in self.book_titles:
-            print(self.book_titles[title.lower()])
-       else:
-            print("Book not found.")
+        book=get_book_title(title)
+        if book:
+            print(book)
+        else:
+            print("Book not found")
 
     def display_books(self):
-        for book in self.books.values():
-                print(book)    
+        books=get_all_books()
+        for book in books:
+            print(book)
 
     def borrow_book(self,user_id,book_id):
-            if book_id not in self.books:
-                print("Book not found")
-                return
-            member=self.members[user_id]
-            book=self.books[book_id]
-            if book.status!="Available":
-                print("Book is not available")
-                return
-            if len(member.borrowed_books)>=member.limit_borrow:
-                print("Borrowing limit reached")
-                return
-            member.borrowed_books.append(book_id)
-            book.status="Borrowed"
-            book.borrowed_by=user_id
-            book.borrow_date=date.today()
-            book.return_by=date.today()+timedelta(days=member.loan_days)
-            print("Book borrowed successfully")
-            borrow_book(book_id,user_id,book.borrow_date,book.return_by)
-            
-    def return_book(self,user_id,book_id):
-        if book_id not in self.books:
+        book=get_book(book_id)
+        member=get_member(user_id)
+        if book==None:
             print("Book not found")
             return
-        member=self.members[user_id]
-        book=self.books[book_id]
-        if book_id not in member.borrowed_books:
+        if book[4]!="Available":
+            print("Book not available")
+            return
+        if member[3]=="Student":
+            borrowed_books=0
+            if borrowed_books>=Student.limit_borrow:
+                print("Borrowing limit reached")
+                return
+            loan_days=Student.loan_days
+        elif member[3]=="Faculty":
+            loan_days=Faculty.loan_days
+        else:
+            print("Librarians cannot borrow books")
+            return
+        borrow_date=date.today()
+        return_by=borrow_date+timedelta(days=loan_days)
+        borrow_book(book_id,user_id,borrow_date,return_by)
+        print("Book borrowed successfully")
+
+    def return_book(self,user_id,book_id):
+        book=get_book(book_id)
+        member=get_member(user_id)
+        if book==None:
+            print("Book not found")
+            return
+        if book[5]!=user_id:
             print("This book was not borrowed")
             return
         today=date.today()
-        if today>book.return_by and member.role=="Student":
-            extra_days=(today-book.return_by).days
-            fine=extra_days*member.fine_each_day
-            member.fine+=fine
-            print("Book returned late.Fine:",fine)
-        member.borrowed_books.remove(book_id)
-        book.status="Available"
-        book.borrowed_by=""
-        book.borrow_date=""
-        book.return_by=""
-        print("Book returned successfully")
+        if today>book[7] and member[3]=="Student":
+            extra_days=(today-book[7]).days
+            fine=extra_days*Student.fine_each_day
+            new_fine=float(member[4])+fine
+            update_member(user_id,member[1],member[2],member[3],new_fine)
+            print("Book returned late. Fine:",fine)
         return_book(book_id)
-
+        print("Book returned successfully")
+        
     def reserve_book(self,user_id,book_id):
-        if book_id not in self.books:
+        book=get_book(book_id)
+        if book==None:
             print("Book not found")
             return
-        member=self.members[user_id]
-        book=self.books[book_id]
-        if book.reserved_by!="":
+        if book[8]!=None:
             print("Book is already reserved")
             return
-        book.reserved_by=user_id
-        member.reserved_books.append(book_id)
-        print("Book reserved successfully")
         reserve_book(book_id,user_id,date.today())
+        print("Book reserved successfully")
 
     def add_member(self,role,name,password):
         self.member_count+=1
@@ -130,42 +125,53 @@ class Library:
         else:
             print("Invalid role")
             return
-        self.members[user_id]=member
-        print("Member added")
         add_member(user_id,name,password,member.role,member.fine)
+        print("Member added")
+        print("User ID:",user_id)
 
     def remove_member(self,user_id):
-        if user_id not in self.members:
+        member=get_member(user_id)
+        if member==None:
             print("Member not found")
         else:
-            del self.members[user_id]
-            print("Member removed successfully")
             remove_member(user_id)
+            print("Member removed successfully")
 
     def search_member(self,user_id):
-        if user_id in self.members:
-            member=self.members[user_id]
-            member.details()
+        member=get_member(user_id)
+        if member:
+            print("userID:",member[0])
+            print("Name:",member[1])
+            print("Role:",member[3])
+            print("fine:",member[4])
         else:
             print("Member not found")
 
-    def pay_fine(self, user_id):
-        member=self.members[user_id]
-        if member.role=="Student":
-            if member.fine==0:
-                print("No fine to be paid")
-            else:
-                print("fine:",member.fine)
-                payment=float(input("Enter amount to pay:"))
-                if payment>member.fine:
-                    print("Payment amount is greater than fine.")
-                elif payment<member.fine:
-                    member.fine-=payment
-                    print("Remaining fine:",member.fine)
-                    pay_fine(user_id,payment,date.today(),member.fine)
-                else:
-                    print("Complete fine paid")
-                    member.fine=0
-                    pay_fine(user_id,payment,date.today(),member.fine)
-        else:
+    def display_members(self):
+        members=get_all_members()
+        for member in members:
+            print(member)
+
+    def pay_fine(self,user_id):
+        member=get_member(user_id)
+        if member==None:
+            print("Member not found")
+            return
+        if member[3]!="Student":
             print("No fine")
+            return
+        if member[4]==0:
+            print("No fine to be paid")
+            return
+        print("Fine:",member[4])
+        payment=float(input("Enter amount to pay:"))
+        if payment>member[4]:
+            print("Payment amount is greater than fine.")
+        elif payment<member[4]:
+            remaining_fine=float(member[4])-payment
+            pay_fine(user_id,payment,remaining_fine)
+            print("Remaining fine:",remaining_fine)
+        else:
+            pay_fine(user_id,payment,0)
+            print("Complete fine paid")
+            
